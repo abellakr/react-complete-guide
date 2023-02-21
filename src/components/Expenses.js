a9ca0927dc687967101e69cb1106d0505d1cf8aa
karlos <karlos>
2023-02-21 13:27:04 -0600

    undo change

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -6,7 +6,7 @@ let Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
-        title={props.items[1].title}
+        title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
       />
